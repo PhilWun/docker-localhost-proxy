3c763d5c0fe02dfcf9b9fa23deafd881aba013e7
Philipp Wundrack <philipp.wundrack@live.de>
2023-02-13 10:08:36 +0100

    add debug messages to start script

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+echo "pwd: $(pwd)"
+
+if [[ -e "Caddyfile"]]; then
+    echo "content of Caddyfile: $(cat Caddyfile)"
+else
+    echo "Caddyfile does not exists."
+fi
+
 # check if environment variable was defined
 if [[ -n "${LOCALHOST_PROXY_PORTS}" ]]; then
+    echo "env var LOCALHOST_PROXY_PORTS is set to ${LOCALHOST_PROXY_PORTS}"
+
     # configure Caddy
     envsubst < Caddyfile | tee Caddyfile # replace placeholders with the values of the respective environment variables
 
+    echo "content of Caddyfile: $(cat Caddyfile)"
+
     caddy start
-fi
+else
+    echo "env var LOCALHOST_PROXY_PORTS is not set"
