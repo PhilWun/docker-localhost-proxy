ca2617b6b5831745de8fa7e6ed0603c409e585bd
Philipp Wundrack <philipp.wundrack@live.de>
2023-02-13 11:20:21 +0100

    add comment

--- start_proxy.sh
+++ start_proxy.sh
@@ -6,7 +6,8 @@ if [[ -n "${LOCALHOST_PROXY_PORTS}" ]]; then
     # replace placeholders with the values of the respective environment variables
     # this needs to be stored to a different file, otherwise a racecondition might occur
     envsubst < Caddyfile.template > Caddyfile
-    export XDG_CONFIG_HOME="$(pwd)"
+    export XDG_CONFIG_HOME="$(pwd)"  # set autosave config location
+
     caddy start
 else
     echo "INFO: env var LOCALHOST_PROXY_PORTS not set, Caddy will not be started"
