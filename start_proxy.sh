796e2261a264b3e5673dcde411f6146696045b66
Philipp Wundrack <philipp.wundrack@live.de>
2023-02-13 11:07:21 +0100

    remove debug commands, rename Caddyfile

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-echo "pwd: $(pwd)"
-
-if [[ -e "Caddyfile" ]]; then
-    echo "content of Caddyfile: $(cat Caddyfile)"
-else
-    echo "Caddyfile does not exists."
-fi
-
 # check if environment variable was defined
 if [[ -n "${LOCALHOST_PROXY_PORTS}" ]]; then
-    echo "env var LOCALHOST_PROXY_PORTS is set to ${LOCALHOST_PROXY_PORTS}"
-
     # configure Caddy
     # replace placeholders with the values of the respective environment variables
     # this needs to be stored to a different file, otherwise a racecondition might occur
-    envsubst < Caddyfile > Caddyfile.tmp
-    mv Caddyfile.tmp Caddyfile
-
-    echo "content of Caddyfile: $(cat Caddyfile)"
-
+    envsubst < Caddyfile.template > Caddyfile
     export XDG_CONFIG_HOME="$(pwd)"
-
     caddy start
 else
-    echo "env var LOCALHOST_PROXY_PORTS is not set"
+    echo "INFO: env var LOCALHOST_PROXY_PORTS not set, Caddy will not be started"
 fi
